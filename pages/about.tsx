51ff641421b0339ac38752f312bc6ecd11e81f9d
Nik Shah <nikps@amazon.com>
2023-01-08 23:29:30 -0700

    Add About Me blurb

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,10 +2,23 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const AboutPage = () => (
-  <Layout title="About Me">
-    <h1>About</h1>
-    <p>This is the about page</p>
-  </Layout>
+    <Layout title="About Me">
+        <h1>About</h1>
+        <p> My name is Nik and I am a web developer and rock climber. I have a passion for all things tech and love the
+            challenge of solving complex problems. In my free time, you can find me at the climbing gym, in the
+            mountains, or setting up the latest silly home automation. In a former life, I was a senior hardware
+            engineer designing server motherboards, but I gave that all up so I could build websites!
+
+            As a frontend developer, I've worked professionally with React and Angular, and while I don't favor one or
+            the other, I strongly prefer Typescript in whatever web project I am working on. I excel when working on
+            teams with a strong collaborative culture, and I get the most joy working with others. I enjoy understanding
+            the  inner works of how the web actually works, and my goal is to eventually be able to lead teams in
+            building scalable, reliable, and usable websites. I also have a strong passion for web accessibility, and
+            believe it is paramount to build inclusive and usable websites for all.
+
+            Thank you for visiting my website. I hope you find it informative and look forward to connecting with
+            you.</p>
+    </Layout>
 )
 
 export default AboutPage
