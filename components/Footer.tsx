93d951dbaa62d49f2aabf0d1c72ce7958172ea60
Nik Shah <nikps@amazon.com>
2023-01-08 21:21:17 -0700

    Project Setup and Footer design

---
+++ components/Footer.tsx
@@ -0,0 +1,77 @@
+import GitHubIcon from "./icons/GitHubIcon";
+import LinkedInIcon from "./icons/LinkedInIcon";
+import React from "react";
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  display: flex;
+  justify-content: center;
+  height: 60px;
+  background: #2d2d2d;
+  color: lightgrey;
+  padding-top: 5px;
+`
+
+const StyledLink = styled.a`
+  border: 1px solid rgba(255, 255, 224, 0.4);
+  border-radius: 4px;
+  padding: 1px 1em;
+  text-decoration: none;
+  color: inherit;
+
+  :hover {
+    border: 1px solid rgba(255, 255, 255, 0.7);;
+  }
+`
+
+const StyledIconMarginText = styled.span`
+  margin-right: .5em;
+`
+
+const MadeByDiv = styled.div`
+  display:flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
+const VerticalDivider = styled.div`
+  color: rgba(255, 255, 255, 0.3);
+`;
+
+const Footer = () => {
+    return(
+    <StyledFooter>
+        <MadeByDiv>
+            <div>Hosted by Vercel</div>
+            <VerticalDivider>&nbsp; | &nbsp;</VerticalDivider>
+            <div>
+                <StyledLink href={"https://github.com/NikS-44/nshah-org"}>
+                    <StyledIconMarginText>Source Code</StyledIconMarginText>
+                    <GitHubIcon/>
+                </StyledLink>
+            </div>
+            <VerticalDivider>&nbsp; | &nbsp;</VerticalDivider>
+            <div>
+                <StyledLink href={"https://github.com/NikS-44/"}>
+                    <StyledIconMarginText>GitHub</StyledIconMarginText>
+                    <GitHubIcon/>
+                </StyledLink>
+            </div>
+            <VerticalDivider>&nbsp; | &nbsp;</VerticalDivider>
+            <div>
+                <StyledLink href={"https://www.linkedin.com/in/nik-shah-657ba616/"}>
+                    <StyledIconMarginText>LinkedIn</StyledIconMarginText>
+                    <LinkedInIcon/>
+                </StyledLink>
+            </div>
+            <VerticalDivider>&nbsp; | &nbsp;</VerticalDivider>
+            <div>
+                © {new Date().getFullYear()}
+            </div>
+        </MadeByDiv>
+    </StyledFooter>
+    )
+}
+
+export default Footer;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,32 +1,48 @@
-import React, { ReactNode } from 'react'
+import React, {ReactNode} from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import styled from "styled-components";
+import Footer from "./Footer";
+
 
 type Props = {
-  children?: ReactNode
-  title?: string
+    children?: ReactNode
+    title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
-        <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
+const SiteContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`
+
+
+
+const PageContent = styled.div`
+  flex-grow: 1;
+`
+
+
+
+const Layout = ({children, title = 'nshah.org'}: Props) => (
+    <SiteContent>
+        <Head>
+            <title>{title}</title>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        </Head>
+        <header>
+            <nav>
+                <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
+                <Link href="/users">Users List</Link> |{' '}
+                <a href="/api/users">Users API</a>
+            </nav>
+        </header>
+        <PageContent>
+            {children}
+        </PageContent>
+        <Footer/>
+    </SiteContent>
 )
 
 export default Layout
